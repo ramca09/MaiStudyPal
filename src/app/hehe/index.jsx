80cf481d0c25c358169f88a6ee6542f77aae5185
ramca09 <ramca0909@gmail.com>
2023-06-29 14:01:39 -0400

    first

---
+++ src/app/hehe/index.jsx
@@ -0,0 +1,3 @@
+export default function hehe() {
+    return <h1>Hello, Next.js!</h1>
+}
---
+++ src/pages/_app.jsx
@@ -0,0 +1,22 @@
+import { Inter } from 'next/font/google';
+import 'styles/globals.css';
+
+// Using next/font instead of a manual setup, we get:
+// - significantly easier setup
+// - automatic best font practices
+// - reduced layout shift
+// - no network requests from the browser
+const primaryFont = Inter({
+  subsets: ['latin'],
+  variable: '--primary-font',
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main className={`${primaryFont.variable} font-sans`}>
+      {/* <Layout> */}
+        <Component {...pageProps} />
+      {/* </Layout> */}
+    </main>
+  );
+}
