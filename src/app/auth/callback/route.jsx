a986d57c1a49e1f8f25949105f2bf8ef845970b6
ramca09 <ramca0909@gmail.com>
2023-07-31 17:36:42 -0400

    auth/callback

--- src/app/auth/callback/route.jsx
+++ src/app/auth/callback/route.jsx
@@ -11,5 +11,5 @@ export async function GET(req) {
         await supabase.auth.exchangeCodeForSession(code);
     }
 
-    return NextResponse.redirect(new URL('/signin', req.url));
+    return NextResponse.redirect(new URL(`${process.env.NEXT_PUBLIC_SITE_URL}/signin`, req.url));
 }
