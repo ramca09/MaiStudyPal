aeae4f737dc0071296b1235514c9a3f44d719e37
ramca09 <ramca0909@gmail.com>
2023-07-30 22:50:17 -0400

    edgeruntime

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -59,4 +59,9 @@ export async function middleware(req) {
 
 export const config = {
   matcher: ["/:path*"],
+  runtime: 'experimental-edge', // for Edge API Routes only
+  unstable_allowDynamic: [
+    // '/lib/utilities.js', // allows a single file
+    '/node_modules/function-bind/**', // use a glob to allow anything in the function-bind 3rd party module
+  ],
 };
